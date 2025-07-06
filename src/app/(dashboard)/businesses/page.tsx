1194a07ababd697cf71f41a5700f909ff59682ba
Christian Murillo <christianoroceomurillo@gmail.com>
2025-07-06 11:20:46 +0800

    Updated businesses module entity and mock data

--- src/app/(dashboard)/businesses/page.tsx
+++ src/app/(dashboard)/businesses/page.tsx
@@ -42,11 +42,51 @@ const columns: DataListColumn[] = [
 ]
 
 const items: Businesses[] = [
-  { id: 1, created_at: "June 1, 2023", name: "Company 1", domain: "www.company1.com" },
-  { id: 2, created_at: "January 12, 2023", name: "Company 2", domain: "www.company2.com" },
-  { id: 3, created_at: "August 27, 2024", name: "Company 3", domain: "www.company3.com" },
-  { id: 4, created_at: "May 17, 2025", name: "Company 4", domain: "www.company4.com" },
-  { id: 5, created_at: "December 7, 2025", name: "Company 5", domain: "www.company5.com" },
+  {
+    id: 1,
+    created_at: "June 1, 2023",
+    name: "Company 1",
+    api_key: "key-company1",
+    domain: "www.company1.com",
+    preferred_timezone: "Asia/Hong Kong",
+    currency: "PHP"
+  },
+  {
+    id: 2,
+    created_at: "January 12, 2023",
+    name: "Company 2",
+    api_key: "key-company2",
+    domain: "www.company2.com",
+    preferred_timezone: "Asia/Hong Kong",
+    currency: "PHP"
+  },
+  {
+    id: 3,
+    created_at: "August 27, 2024",
+    name: "Company 3",
+    api_key: "key-company3",
+    domain: "www.company3.com",
+    preferred_timezone: "Asia/Hong Kong",
+    currency: "PHP"
+  },
+  {
+    id: 4,
+    created_at: "May 17, 2025",
+    name: "Company 4",
+    api_key: "key-company4",
+    domain: "www.company4.com",
+    preferred_timezone: "Asia/Hong Kong",
+    currency: "PHP"
+  },
+  {
+    id: 5,
+    created_at: "December 7, 2025",
+    name: "Company 5",
+    api_key: "key-company5",
+    domain: "www.company5.com",
+    preferred_timezone: "Asia/Hong Kong",
+    currency: "PHP"
+  },
 ];
 
 export default function BusinessePage() {
--- src/entities/businesses.ts
+++ src/entities/businesses.ts
@@ -1,6 +1,12 @@
 export type Businesses = {
   id: number;
   name: string;
+  api_key: string;
+  domain?: string;
+  logo_path?: string;
+  preferred_timezone?: string;
+  currency?: string;
   created_at: string;
-  domain: string;
+  updated_at?: string;
+  deleted_at?: string;
 };
